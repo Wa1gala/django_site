2d7cc1b085ae65bf7018af137e2f44c6c58e2e1d
Wa1gala <zaquz111@gmail.com>
2021-12-21 16:43:17 +0300

    init: firs

---
+++ mainapp/tamplatetags/specifications.py
@@ -0,0 +1,46 @@
+from django import template
+
+from django.utils.safestring import mark_safe
+
+register = template.Library()
+
+TABLE_HEAD = """ 
+                <table class="table">
+                    <tbody>
+            """
+
+TABLE_TAIL = """
+                    </tbody>
+                </table>
+            """
+
+TABLE_CONTENT = """
+                    <tr>
+                      <th>{name}</th>
+                      <td>{value}</td>
+                    </tr>  
+                """
+
+PRODUCT_SPEC = {
+    'sneakers': {
+        'Размер': 'size_sneakers',
+        'Бренд': 'brand_name',
+    },
+    'tshirt': {
+        'Размер': 'size_tshirt',
+        'Бренд': 'brand_name',
+    },
+}
+
+
+def get_product_spec(product, model_name):
+    table_content = ''
+    for name, value in PRODUCT_SPEC[model_name].items():
+        table_content += TABLE_CONTENT.format(name=name, value=getattr(product, value))
+    return table_content
+
+
+@register.filter
+def product_spec(product):
+    model_name = product.__class__._meta.model_name
+    return mark_safe(TABLE_HEAD + get_product_spec(product, model_name) + TABLE_TAIL)
